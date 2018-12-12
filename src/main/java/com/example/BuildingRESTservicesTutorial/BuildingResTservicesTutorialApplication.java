3f1a101cf87f639312a0890787419439229af671
Tim0Xa <kogtim@gmial.com>
2018-12-12 13:59:38 +0200

    Upload project

---
+++ src/main/java/com/example/BuildingRESTservicesTutorial/BuildingResTservicesTutorialApplication.java
@@ -0,0 +1,12 @@
+package com.example.BuildingRESTservicesTutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingResTservicesTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingResTservicesTutorialApplication.class, args);
+	}
+}
